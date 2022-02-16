e0d6f9cbc4c7744bd789efd3c14273c1c9a732a2
rajdeep sahoo <rajdeepsahoo2012@gmail.com>
2022-02-16 06:19:31 +0530

    comment

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,56 @@ public class Main {
         list.add(78);
         printAllNumbersInList(list);
         printAllNumbersInListStream(list); //using stream and method reference
+        printOddNumbersInListStream(list);
 
+        List<String>  strlist=new ArrayList<String>();
+        strlist.add("aws");
+        strlist.add("docker");
+        strlist.add("kubernetis");
+        strlist.add("spring boot");
+        
+        printAllStringIndividually(strlist);
+        printStringStsrtsWithSprig(strlist);
+        printStringLengthMoreThanFour(strlist);
+    }
+
+    private static void printStringLengthMoreThanFour(List<String> strlist) {
+        strlist.stream().filter(str->str.length()>4).forEach(System.out::println);
+    }
+
+    private static void printStringStsrtsWithSprig(List<String> strlist) {
+        strlist.stream().filter(str->str.contains("spring")).forEach(System.out::println);
+    }
+
+    private static void printAllStringIndividually(List<String> strlist) {
+        strlist.stream().forEach(System.out::println);
+    }
+
+    private static void printOddNumbersInListStream(List<Integer> list) {
+        list.stream().filter(number->number%2==1).forEach(System.out::println);
     }
 
     private static void printAllNumbersInListStream(List<Integer> list) {
         for(int num:list){
+            if(num%2==0)
             System.out.println("Non stream ouput :: "+num);
         }
     }
 
     private static void printAllNumbersInList(List<Integer> list) {
-        list.stream().forEach(Main::print); //Method reference
+       // list.stream().forEach(Main::print); //Method reference
+       // list.stream().forEach(System.out::println);
+       // list.stream().filter(Main :: isEven).forEach(System.out::println);
+
+        list.stream().filter(num->num%2==0).forEach(System.out::println);
+
     }
 
-    private static void print(Integer integer) {
-        System.out.println("Stream output : "+integer);
-    }
+//    private static boolean isEven(int number){
+//        return number%2==0;
+//    }
+
+//    private static void print(Integer integer) {
+//        System.out.println("Stream output : "+integer);
+//    }
 }
